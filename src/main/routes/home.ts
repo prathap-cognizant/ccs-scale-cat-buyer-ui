0b69624b67eb5792fc963bc49bf195af3209ed9c
pradhap87 <89925857+prathap-cognizant@users.noreply.github.com>
2021-09-17 14:01:36 +0100

    Base code

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,10 +1,13 @@
 import { Application } from 'express';
+const fs = require('fs')
+const baseUrl = "src/main/resources/JSON/"
 
 export default function(app: Application): void {
 
   app.get('/', (req, res) => {
-
-    res.render('home');
+    let rawdata = fs.readFileSync(baseUrl+'landingPage.json')
+    let jsonObj = JSON.parse(rawdata)
+    res.render('home', {jsonObj: jsonObj});
   });
 
 }
